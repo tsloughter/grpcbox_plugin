0f99b0b296d72d5574bbfb55a147e1d8baaea5a3
Tristan Sloughter <t@crashfast.com>
2017-12-03 14:43:58 -0800

    change name to _bhvr

--- priv/grpcbox_service.erl
+++ priv/grpcbox_service.erl
@@ -5,11 +5,11 @@
 
 %% this module was generated on {{datetime}} and should not be modified manually
 
--module({{service_name}}_behaviour).
+-module({{service_name}}_bhvr).
 
 {{#methods}}
 %% @doc {{^input_stream}}{{^output_stream}}Unary RPC{{/output_stream}}{{/input_stream}}
--callback {{method}}({{^input_stream}}{{#output_stream}}grpcbox_stream:t(), {{pb_module}}:'{{input}}'(){{/output_stream}}{{/input_stream}}{{#input_stream}}{{^output_stream}}grpcbox_stream:t(){{/output_stream}}{{#output_stream}}grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{^input_stream}}{{^output_stream}}ctx:ctx(), {{pb_module}}:'{{input}}'(){{/output_stream}}{{/input_stream}}) ->
+-callback {{method}}({{^input_stream}}{{#output_stream}}{{pb_module}}:'{{input}}'(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{#input_stream}}{{^output_stream}}reference(), grpcbox_stream:t(){{/output_stream}}{{#output_stream}}reference(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{^input_stream}}{{^output_stream}}ctx:ctx(), {{pb_module}}:'{{input}}'(){{/output_stream}}{{/input_stream}}) ->
     {{#output_stream}}ok{{/output_stream}}{{^output_stream}}{ok, {{pb_module}}:'{{output}}'()}{{/output_stream}} | {error, term()}.
 
 {{/methods}}
