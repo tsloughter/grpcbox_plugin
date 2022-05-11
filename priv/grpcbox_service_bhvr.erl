1d117e72d48560d48238950e9c469800a0b13aae
Tomas Abrahamsson <tomas.abrahamsson@gmail.com>
2022-05-11 14:19:10 +0200

    Fix template doc tags
    -callbacks must not have @doc tags.

--- priv/grpcbox_service_bhvr.erl
+++ priv/grpcbox_service_bhvr.erl
@@ -8,7 +8,7 @@
 -module({{module_name}}_bhvr).
 
 {{#methods}}
-%% @doc {{^input_stream}}{{^output_stream}}Unary RPC{{/output_stream}}{{/input_stream}}
+%% {{^input_stream}}{{^output_stream}}Unary RPC{{/output_stream}}{{/input_stream}}
 -callback {{method}}({{^input_stream}}{{#output_stream}}{{pb_module}}:{{input}}(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{#input_stream}}{{^output_stream}}reference(), grpcbox_stream:t(){{/output_stream}}{{#output_stream}}reference(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{^input_stream}}{{^output_stream}}ctx:ctx(), {{pb_module}}:{{input}}(){{/output_stream}}{{/input_stream}}) ->
     {{#output_stream}}ok{{/output_stream}}{{^output_stream}}{ok, {{pb_module}}:{{output}}(), ctx:ctx()}{{/output_stream}} | grpcbox_stream:grpc_error_response().
 
